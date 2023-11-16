de15b555b76a9241132337628b4a5f20d6ee3469
kayFley <vasya.shumilov.2003@gmail.com>
2023-11-16 18:43:36 +0700

    add motion

--- src/app/components/MenuOverlay.jsx
+++ src/app/components/MenuOverlay.jsx
@@ -1,15 +1,23 @@
+"use client";
 import React from "react";
 import NavLink from "./NavLink";
+import { motion } from "framer-motion";
 
 const MenuOverlay = ({ links }) => {
     return (
-        <ul className="flex flex-col py-4 items-center">
-            {links.map((link, index) => (
-                <li key={index}>
-                    <NavLink href={link.path} title={link.title} />
-                </li>
-            ))}
-        </ul>
+        <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{ duration: 0.2 }}
+        >
+            <ul className="flex flex-col py-4 items-center">
+                {links.map((link, index) => (
+                    <li key={index}>
+                        <NavLink href={link.path} title={link.title} />
+                    </li>
+                ))}
+            </ul>
+        </motion.div>
     );
 };
 
