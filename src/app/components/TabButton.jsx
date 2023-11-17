4deb7498557df704b85564b605314358c13e4588
kayFley <vasya.shumilov.2003@gmail.com>
2023-11-17 21:49:20 +0700

    Added an animated selection

--- src/app/components/TabButton.jsx
+++ src/app/components/TabButton.jsx
@@ -1,15 +1,26 @@
 import React from "react";
+import { motion } from "framer-motion";
+
+const variants = {
+    default: { width: 0 },
+    active: { width: "calc(93% - 0.75rem)" },
+};
 
 const TabButton = ({ active, selectTab, children }) => {
-    const buttonClasses = active ? "text-white" : "text-[#ADB7BE]";
+    const buttonClasses = active ? "text-white" : "text-[#676d71]";
 
     return (
         <button onClick={selectTab}>
             <p
-                className={`mr-3 font-semibold hover:text-white ${buttonClasses}`}
+                className={`mr-6 font-semibold hover:text-white ${buttonClasses}`}
             >
                 {children}
             </p>
+            <motion.div
+                animate={active ? "active" : "default"}
+                variants={variants}
+                className="h-0.5 bg-white mt-1"
+            ></motion.div>
         </button>
     );
 };
